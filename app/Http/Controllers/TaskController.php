63a10b848c41914819e52ce7c4c32f441d4cff1e
Ruditya <rudityap20@gmail.com>
2020-03-29 13:59:41 +0700

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    function pypart($n) 
+{ 
+      
+    for($i=0;$i<=9;$i++){
+ 
+        for ($d=10-$i; $d > 0; $d--)  {
+     
+            echo "&nbsp;&nbsp;";
+        }
+        for($j=1;$j<=$i;$j++){
+     
+            echo "&nbsp;".$i."&nbsp;";
+        }
+     
+        echo "<br>";
+     
+    }
+  
+   
+}}
